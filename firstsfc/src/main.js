d58d72217dd73c6db6518a1610dd67af7592b3f8
Jan Samuel G. Sultan <jgsultan@student.apc.edu.ph>
2026-01-31 22:53:27 +0000

    Fixed

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,17 +4,6 @@ import App from './App.vue'
 import Login from './views/Login.vue'
 import Dashboard from './views/Dashboard.vue'
 
-router.beforeEach(async (to, from, next) => {
-  const { data: { session } } = await supabase.auth.getSession();
-
-  if (to.path === '/dashboard' && !session) {
-    // If trying to access dashboard without login, send to login
-    next('/');
-  } else {
-    next();
-  }
-});
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -26,3 +15,14 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+router.beforeEach(async (to, from, next) => {
+  const { data: { session } } = await supabase.auth.getSession();
+
+  if (to.path === '/dashboard' && !session) {
+    // If trying to access dashboard without login, send to login
+    next('/');
+  } else {
+    next();
+  }
+});
